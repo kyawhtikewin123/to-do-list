6716fdb3457a90bd08e2be34b94ebf44259cc625
kyaw htike win <kyawhtikewin784@gmail.com>
2022-11-17 11:19:15 +0630

    final

---
+++ script.js
@@ -0,0 +1,25 @@
+$(".txtb").on("keyup",function(e){
+    if(e.keyCode == 13 && $(".txtb").val() != "")
+    {
+      var task = $("<div class='task'></div>").text($(".txtb").val());
+      var del = $("<i class='fas fa-trash-alt text-danger'></i>").click(function(){
+        var p = $(this).parent();
+        p.fadeOut(function(){
+          p.remove();
+        });
+      });
+
+      var check = $("<i class='fa-sharp fa-solid fa-square-check text-success'></i>").click(function(){
+        var p = $(this).parent();
+        p.fadeOut(function(){
+          $(".comp").append(p);
+          p.fadeIn();
+        });
+        $(this).remove();
+      });
+
+      task.append(del,check);
+      $(".notcomp").append(task);
+      $(".txtb").val("");
+    }
+  });
